21df438029fdc4bda4028523da1c6291e5ea2506
Isaac Kilbourne <isaac@kilbourne.ca>
2024-11-17 18:12:40 -0500

    feat: better drawer list items

--- app/src/main/java/ca/kilbourne/isaac/lysts/ui/components/TodoListList.kt
+++ app/src/main/java/ca/kilbourne/isaac/lysts/ui/components/TodoListList.kt
@@ -1,6 +1,8 @@
 package ca.kilbourne.isaac.lysts.ui.components
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -8,15 +10,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Place
 import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material.icons.outlined.ShoppingCart
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,15 +53,14 @@ fun PreviewTodoListList() {
 fun TodoListList(lists: MutableList<TodoList>, onListSelected: (TodoList) -> Unit = {}) {
     LazyColumn {
         itemsIndexed(lists) { _, list ->
-            TextButton(
-                debounced { onListSelected(list) },
-                shape = RectangleShape,
-                colors = ButtonDefaults.textButtonColors(
-                    Color.Transparent, MaterialTheme.colorScheme.onBackground
-                )
+            Row(
+                Modifier
+                    .debouncedClickable() { onListSelected(list) }
+                    .minimumInteractiveComponentSize()
+                    .padding(16.dp, 0.dp)
             ) {
                 Icon(list.icon, contentDescription = null)
-                Spacer(Modifier.width(8.dp))
+                Spacer(Modifier.width(16.dp))
                 Text(
                     text = list.name, textAlign = TextAlign.Start, modifier = Modifier.weight(1f)
                 )
--- app/src/main/java/ca/kilbourne/isaac/lysts/ui/presentation/main/MainActivityDrawer.kt
+++ app/src/main/java/ca/kilbourne/isaac/lysts/ui/presentation/main/MainActivityDrawer.kt
@@ -1,19 +1,15 @@
 package ca.kilbourne.isaac.lysts.ui.presentation.main
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import ca.kilbourne.isaac.lysts.data.Placeholder
 import ca.kilbourne.isaac.lysts.data.TodoList
 import ca.kilbourne.isaac.lysts.ui.components.TodoListList
@@ -35,18 +31,13 @@ private fun PreviewMainActivityDrawer() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainActivityDrawer(todoLists: MutableList<TodoList>) {
-    TopAppBar(title = { Text("Your Lysts") }, actions = {
-        IconButton(onClick = {}) {
-            Icon(Icons.Outlined.Add, contentDescription = "Add List")
-        }
-    })
-    Row {
-        Text(
-            "Your Lysts",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(16.dp, 16.dp, 0.dp, 8.dp)
-        )
+    Column {
+        TopAppBar(title = { Text("Your Lysts") }, actions = {
+            IconButton(onClick = {}) {
+                Icon(Icons.Outlined.Add, contentDescription = "Add List")
+            }
+        })
+        
+        TodoListList(todoLists)
     }
-
-    TodoListList(todoLists)
 }
