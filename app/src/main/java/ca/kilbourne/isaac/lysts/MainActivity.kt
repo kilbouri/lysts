ba57f959741f598d46db7ab5d143ce8a99b9f5e9
Isaac Kilbourne <isaac@kilbourne.ca>
2025-01-24 23:16:56 -0500

    feat: add app menu

--- app/src/main/java/ca/kilbourne/isaac/lysts/MainActivity.kt
+++ app/src/main/java/ca/kilbourne/isaac/lysts/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import ca.kilbourne.isaac.lysts.data.TodoList
@@ -24,10 +27,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
+            var appMenuShown by remember { mutableStateOf(false) }
+
             val allLists by viewModel.todoLists.getAll().collectAsStateWithLifecycle(listOf())
 
             LystsTheme {
-                Scaffold(topBar = { MainActivityTopBar(TodoList(name = "Shopping List")) }) {
+                Scaffold(topBar = {
+                    MainActivityTopBar(
+                        TodoList(name = "Shopping List"),
+                        showListOptions = { appMenuShown = true })
+                }
+                ) {
                     Box(Modifier.padding(it))
                 }
             }
--- app/src/main/java/ca/kilbourne/isaac/lysts/ui/presentation/main/MainActivityTopBar.kt
+++ app/src/main/java/ca/kilbourne/isaac/lysts/ui/presentation/main/MainActivityTopBar.kt
@@ -1,19 +1,35 @@
 package ca.kilbourne.isaac.lysts.ui.presentation.main
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ca.kilbourne.isaac.lysts.data.TodoList
@@ -27,24 +43,103 @@ fun MainActivityTopBarPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainActivityTopBar(
-    currentList: TodoList, showListPicker: () -> Unit = {}, showListOptions: () -> Unit = {}
+    currentList: TodoList,
+    showListPicker: () -> Unit = {},
+    showListOptions: () -> Unit = {},
+    onDismissRequest: () -> Unit = {},
+    onRemoveDoneRequest: () -> Unit = {},
+    onRemoveAllRequest: () -> Unit = {},
+    onNewListRequest: () -> Unit = {},
+    onEditListRequest: () -> Unit = {},
+    onDeleteListRequest: () -> Unit = {},
+    onSettingsRequest: () -> Unit = {}
 ) {
-    CenterAlignedTopAppBar(navigationIcon = {
-        IconButton(onClick = showListPicker) {
-            Icon(Icons.Default.Menu, contentDescription = "Lists Menu")
-        }
-    }, title = {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                Icons.Default.ShoppingCart,
-                contentDescription = "Icon for List '${currentList.name}'"
-            )
+    var appMenuExpanded by remember { mutableStateOf(false) }
 
-            Text(currentList.name, modifier = Modifier.padding(start = 8.dp))
+    CenterAlignedTopAppBar(
+        navigationIcon = {
+            IconButton(onClick = showListPicker) {
+                Icon(Icons.Default.Menu, contentDescription = "Lists Menu")
+            }
+        },
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = "Icon for List '${currentList.name}'"
+                )
+
+                Text(currentList.name, modifier = Modifier.padding(start = 8.dp))
+            }
+        },
+        actions = {
+            Box() {
+                IconButton(onClick = { appMenuExpanded = !appMenuExpanded }) {
+                    Icon(Icons.Default.MoreVert, "More")
+                }
+
+                MainActivityAppMenu(
+                    expanded = appMenuExpanded,
+                    onDismissRequest = { appMenuExpanded = false },
+                    onRemoveDoneRequest = onRemoveDoneRequest,
+                    onRemoveAllRequest = onRemoveAllRequest,
+                    onNewListRequest = onNewListRequest,
+                    onEditListRequest = onEditListRequest,
+                    onDeleteListRequest = onDeleteListRequest,
+                    onSettingsRequest = onSettingsRequest
+                )
+            }
         }
-    }, actions = {
-        IconButton(onClick = showListOptions) {
-            Icon(Icons.Default.MoreVert, "More")
-        }
-    })
+    )
+}
+
+@Composable
+private fun MainActivityAppMenu(
+    expanded: Boolean,
+    onDismissRequest: () -> Unit = {},
+    onRemoveDoneRequest: () -> Unit = {},
+    onRemoveAllRequest: () -> Unit = {},
+    onNewListRequest: () -> Unit = {},
+    onEditListRequest: () -> Unit = {},
+    onDeleteListRequest: () -> Unit = {},
+    onSettingsRequest: () -> Unit = {}
+) {
+    @Composable
+    fun AppMenuItem(
+        icon: ImageVector,
+        label: String,
+        onClick: () -> Unit,
+        color: Color = Color.Unspecified
+    ) {
+        val iconTint = if (color == Color.Unspecified) LocalContentColor.current else color
+
+        DropdownMenuItem(
+            leadingIcon = { Icon(icon, null, tint = iconTint) },
+            text = { Text(label, color = color) },
+            onClick = onClick
+        )
+    }
+
+    DropdownMenu(expanded, onDismissRequest) {
+        AppMenuItem(
+            Icons.Default.Check,
+            "Remove Done",
+            onClick = onRemoveDoneRequest
+        )
+        AppMenuItem(
+            Icons.Outlined.Delete,
+            "Remove All",
+            onClick = onRemoveAllRequest
+        )
+
+        HorizontalDivider(modifier = Modifier.padding(8.dp, 0.dp))
+
+        AppMenuItem(Icons.Default.Add, "New List", onClick = onNewListRequest)
+        AppMenuItem(Icons.Default.Edit, "Edit List", onClick = onEditListRequest)
+        AppMenuItem(Icons.Outlined.Delete, "Delete List", onDeleteListRequest, color = Color.Red)
+
+        HorizontalDivider(modifier = Modifier.padding(8.dp, 0.dp))
+
+        AppMenuItem(Icons.Outlined.Settings, "Settings", onClick = onSettingsRequest)
+    }
 }
